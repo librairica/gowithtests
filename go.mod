39a923d8ab2843278c93ab55dfe81bdabda99453
librairica <erica.handelman@gmail.com>
2022-10-05 14:33:45 -0400

    go with tests helloworld and integers

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/librairica/integers
+
+go 1.19
---
+++ helloworld/helloworld.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	// fmt.Println(Hello("world", ""))
+	fmt.Println(strings.ToUpper(Hello("Erica", "")))
+}
